baded96ba94ab6bd704e9de231af71052b0e9259
Fallen-Angel-FF <zhamichenok@gmail.com>
2022-02-01 00:22:43 +0300

    Добавьте файлы проекта.

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EmpComp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee {Name = "Tom",SurName="Averen",Patronymic="Hugon",Age=28},
+                    new Employee {Name = "Alice",SurName="Marol",Patronymic="Markes", Age = 26 },
+                    new Employee {Name = "Sam",SurName="Rafian",Patronymic="Jameson",Age = 28 }
+            );
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmpComp.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+
+    }
+}
