c35f93c010b79b8e58773b566e5be804aa5a29d6
Fallen-Angel-FF <zhamichenok@gmail.com>
2022-02-01 17:15:47 +0300

    added Employee Requests and Responses ended EmployeeController

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EmpComp.Repositories;
+using EmpComp.ReqRes.EmployeesService.Request;
+using EmpComp.ReqRes.EmployeesService.Response;
+using Microsoft.EntityFrameworkCore;
 namespace EmpComp.Controllers
 {
     [ApiController]
@@ -13,30 +16,118 @@ namespace EmpComp.Controllers
             _mainRepository = mainRepository;
         }
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<GetAllEmployeesResponse>> Get()
         {
-           
+            var employees = await _mainRepository.EmployeeRepository.GetAll().ToListAsync();
+            if (employees == null || employees.Count==0)
+                return NotFound("There is no employees.");
+
+            var employeesResponse = new List<GetOneEmployeeResponse>();
+            foreach (var employee in employees)
+                employeesResponse.Add(new GetOneEmployeeResponse
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    SurName = employee.SurName,
+                    Patronymic=employee.Patronymic,
+                    Age = employee.Age
+                });
+
+            return Ok(new GetAllEmployeesResponse
+            {
+                Employees = employeesResponse
+            });
         }
 
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<GetOneEmployeeResponse>> Get(Guid id)
         {
-            return "value";
+            var employee = await _mainRepository.EmployeeRepository
+                                                .Find(e => e.Id == id).FirstOrDefaultAsync();
+            if (employee == null)
+                return NotFound($"There is no employee with {id} id.");
+
+            return Ok(new GetOneEmployeeResponse
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                SurName = employee.SurName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            });
+
         }
 
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<CreateEmployeeResponse>> Create([FromBody] CreateEmployeeRequest request)
         {
+            var employee = _mainRepository.EmployeeRepository
+                                        .Find(e => e.Name == request.Name && e.SurName == request.SurName
+                                              && e.Patronymic == request.Patronymic && e.Age == request.Age);
+            if (employee != null) return Problem("Employee with this data already exists.");
+
+            Employee createdEmployee = new Employee
+            {
+                Name = request.Name,
+                SurName = request.SurName,
+                Patronymic = request.Patronymic,
+                Age = request.Age
+            };
+            await _mainRepository.EmployeeRepository.CreateAsync(createdEmployee);
+            await _mainRepository.SaveChangesAsync();
+            
+            return Ok(new CreateEmployeeResponse
+            {
+                Employee = new GetOneEmployeeResponse
+                {
+                    Id=createdEmployee.Id,
+                    Name = createdEmployee.Name,
+                    SurName= createdEmployee.SurName,
+                    Patronymic= createdEmployee.Patronymic,
+                    Age=createdEmployee.Age
+                }
+            });
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<UpdateEmployeeResponse>> Put([FromBody] UpdateEmployeeRequest request)
         {
+            var employee = await _mainRepository.EmployeeRepository.Find(e => e.Id == request.Id).FirstOrDefaultAsync();
+            if (employee == null) 
+                return Problem($"Employee with {request.Id} id does not exist.");
+
+            employee.Name=request.Name;
+            employee.SurName=request.SurName;
+            employee.Patronymic=request.Patronymic;
+            employee.Age=request.Age;
+
+            await _mainRepository.EmployeeRepository.UpdateAsync(employee);
+            await _mainRepository.SaveChangesAsync();
+
+            return Ok(new UpdateEmployeeResponse 
+            {
+                Employee= new GetOneEmployeeResponse
+                {
+                    Id=employee.Id,
+                    Name=employee.Name,
+                    SurName=employee.SurName,
+                    Patronymic=employee.Patronymic,
+                    Age=employee.Age
+                }
+            });
         }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
         {
+            var employee = _mainRepository.EmployeeRepository.Find(e => e.Id == id)
+                                                             .FirstOrDefault();
+            if (employee==null) return NotFound($"Employee with {id} id not found");
+
+            await _mainRepository.EmployeeRepository.DeleteAsync(employee);
+            await _mainRepository.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
